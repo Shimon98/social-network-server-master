bb67f2dfa5429062d7a63fcf799722b2b6ce1e52
diana-ab <dianabruk@gmail.com>
2026-03-22 16:18:10 +0200

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,41 +1,42 @@
-create table if not exists users
+create table users
 (
-    id            int auto_increment
+    id                bigint auto_increment
         primary key,
-    username      varchar(50)  not null,
-    email         varchar(100) not null,
-    password_hash varchar(255) not null,
+    username          varchar(50)  not null,
+    email             varchar(100) not null,
+    password_hash     varchar(255) not null,
+    profile_image_url varchar(500) null,
     constraint email
         unique (email),
     constraint username
         unique (username)
 );
 
-create table if not exists posts
+create table posts
 (
-    id         int auto_increment
+    id         bigint auto_increment
         primary key,
-    user_id    int                                not null,
+    user_id    bigint                              not null,
     content    text                                not null,
     created_at timestamp default CURRENT_TIMESTAMP null,
     constraint posts_ibfk_1
         foreign key (user_id) references users (id)
 );
 
-create index if not exists user_id
+create index user_id
     on posts (user_id);
 
-create table if not exists refresh_tokens
+create table refresh_tokens
 (
-    id         int auto_increment
+    id         bigint auto_increment
         primary key,
-    user_id    int          not null,
+    user_id    bigint       not null,
     token      varchar(500) not null,
     expires_at bigint       not null,
     created_at bigint       not null,
     constraint token
         unique (token),
-    constraint refresh_tokens_ibfk_1
+    constraint `refresh_tokens_ibfk_1;`
         foreign key (user_id) references users (id)
 );
 
